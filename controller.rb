a855d0cfb7e8e8e2c36164e3677e04cf5f127753
Ernest Fraiman <upapaus@gmail.com>
2018-11-06 16:44:11 +0000

    Finished all extensions

--- controller.rb
+++ controller.rb
@@ -3,10 +3,20 @@ require('sinatra/contrib/all') if development?
 require_relative('./models/rpsgame')
 also_reload('./models/*')
 
-get '/check_win/:hand1/:hand2' do
-  first_num = params[:hand1].to_s
-  second_num = params[:hand2].to_s
-  new_game = RPSGame.new()
-  @game_result = RPSGame.check_win(first_num, second_num)
+get '/check_win/:hand1' do
+  first_hand = params[:hand1].to_s
+  @game_result = RPSGame.check_win(first_hand)
   erb( :result )
+end
+
+get '/check_win2/:hand1/:hand2' do
+  first_hand = params[:hand1].to_s
+  second_hand = params[:hand2].to_s
+  @game_result = RPSGame.check_win2(first_hand, second_hand)
+  erb( :result )
+end
+
+
+get '/welcome' do
+  erb (:welcome)
 end
--- specs/rpsgame_spec.rb
+++ specs/rpsgame_spec.rb
@@ -5,12 +5,17 @@ require_relative('../models/rpsgame')
 class CustomerTest < MiniTest::Test
 
   def test_win
-    expected = RPSGame.check_win("rock", "paper")
+    expected = RPSGame.check_win("rock")
     assert_equal("Player 2 wins with paper!", expected)
   end
 
   def test_draw
-    expected = RPSGame.check_win("rock", "rock")
+    expected = RPSGame.check_win("paper")
+    assert_equal("It's a Draw!", expected)
+  end
+
+  def test_two_human_draw
+    expected = RPSGame.check_win2("paper", "paper")
     assert_equal("It's a Draw!", expected)
   end
 
